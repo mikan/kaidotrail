85b6920b6ed5ef45c4656cd26b205386cf95809e
Yutaka Kato <mikan@aomikan.org>
2026-03-06 18:30:26 +0900

    feat: add nakasendo spots

---
+++ public/spot_nihonbashi.js
@@ -0,0 +1,24 @@
+const nihonbashiSpot = {
+  name: "日本橋",
+  kana: "にほんばし",
+  coordinate: [35.684112, 139.774533],
+  icon: "bridge",
+  description:
+    "五街道の一里塚の起点であり、旧街道の歩き旅の道中では何度もその名前を目にすることになります。" +
+    "橋の中央に国道の起点を示す日本国道路元標が埋まっていますが、道路のど真ん中なので安全に見学することは難しいです。" +
+    "北詰に先代の道路元標 (都電の架線柱) や原稿の道路元標のレプリカが展示されています。",
+  pictures: [
+    {
+      url: "https://kaidotrail.github.io/img/kagaorange/2022/12-04/IMG_20221204_051810.webp",
+      comment: "南西から見た日本橋",
+    },
+    {
+      url: "https://kaidotrail.github.io/img/kagaorange/2022/12-04/IMG_20221204_052134.webp",
+      comment: "現行の日本国道路元標のレプリカ",
+    },
+    {
+      url: "https://kaidotrail.github.io/img/kagaorange/2022/12-04/IMG_20221204_052010.webp",
+      comment: "麒麟像",
+    },
+  ],
+};
